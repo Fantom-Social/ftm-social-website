49c5236dc7cab8871ab76637b9da31c352fe87fb
henrygagnier <prosock@protonmail.com>
2023-03-19 10:44:38 -0400

    modified:   src/social/misc/navbar/navbar.js

--- src/social/misc/navbar/navbar.js
+++ src/social/misc/navbar/navbar.js
@@ -1,10 +1,13 @@
 import URL from "../../../constants/websiteURL"
 import { Component } from "react"
+import sendError from "../notifications/error";
 const {ethereum} = window;
 export default class Navbar extends Component {
     constructor(props) {
         super(props)
         this.state = {loggedIn : <></>}
+        if (typeof window.ethereum !== 'undefined') {
+           
         ethereum.request({method: 'eth_accounts'}).then((accounts) => {
             if (accounts.length > 0) {
                 this.setState({loggedIn : <><a className="navOption" href={URL + "app/profile/" + accounts[0]}>Profile</a>
@@ -12,6 +15,7 @@ export default class Navbar extends Component {
             }
         })
     }
+}
     render() {
         return (
             <div className="navOptions">
